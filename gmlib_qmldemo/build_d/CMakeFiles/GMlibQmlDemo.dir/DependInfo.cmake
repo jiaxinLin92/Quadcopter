a632a981e451e910b063b5119e71a4f0f51f9bc9
jiaxinLin92 <linjx.jessie@gmail.com>
2016-09-07 11:01:33 +0200

    begin

---
+++ gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/quadcopter/gmlib_qmldemo/application/fboinsgrenderer.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/fboinsgrenderer.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/application/gmlibwrapper.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/gmlibwrapper.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/application/guiapplication.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/guiapplication.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/application/main.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/main.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/application/moc_fboinsgrenderer.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/moc_fboinsgrenderer.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/application/moc_gmlibwrapper.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/moc_gmlibwrapper.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/application/moc_guiapplication.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/moc_guiapplication.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/application/moc_window.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/moc_window.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/application/window.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/application/window.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/body.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/body.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/defaulthidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/defaulthidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidaction.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidaction.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidbinding.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidbinding.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidinput.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidinput.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidinputevent.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidinputevent.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidkbmouseinput.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidkbmouseinput.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/hidmanagertreemodel.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/hidmanagertreemodel.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/hidmanager/moc_defaulthidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/moc_defaulthidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/hidmanager/moc_hidaction.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/moc_hidaction.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/hidmanager/moc_hidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/moc_hidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/hidmanager/moc_hidmanagertreemodel.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/moc_hidmanagertreemodel.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/hidmanager/moc_standardhidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/moc_standardhidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/hidmanager/standardhidmanager.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/hidmanager/standardhidmanager.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/moc_scenario.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/moc_scenario.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/plane.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/plane.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/build_d/qrc_application.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/qrc_application.cpp.obj"
+  "C:/quadcopter/gmlib_qmldemo/scenario.cpp" "C:/quadcopter/gmlib_qmldemo/build_d/CMakeFiles/GMlibQmlDemo.dir/scenario.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GM_GL_DEBUG"
+  "GM_STREAM"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/quadcopter/gmlib/build_dinfo/include"
+  "C:/Program Files (x86)/glew/include"
+  "C:/Qt/5.7/mingw53_32/include"
+  "C:/Qt/5.7/mingw53_32/include/QtCore"
+  "C:/Qt/5.7/mingw53_32/./mkspecs/win32-g++"
+  "C:/Qt/5.7/mingw53_32/include/QtQuick"
+  "C:/Qt/5.7/mingw53_32/include/QtQml"
+  "C:/Qt/5.7/mingw53_32/include/QtNetwork"
+  "C:/Qt/5.7/mingw53_32/include/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
